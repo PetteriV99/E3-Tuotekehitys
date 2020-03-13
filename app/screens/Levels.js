1b31e4da859810151b7bf65d0c8dd432e7201152
apbd <60779371+apbd@users.noreply.github.com>
2020-03-13 12:57:43 +0200

    Update Levels.js

--- app/screens/Levels.js
+++ app/screens/Levels.js
@@ -3,9 +3,19 @@ import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-nativ
 
 
 export default function Levels({navigation}){
+
+    const pressLevel = () => {
+        navigation.navigate("Level")
+    }
+
     return (
         <View style={styles.container}>
-            
+            <TouchableOpacity 
+                style={styles.floatingButton2}
+                onPress={pressLevel}
+                >
+            <Text style={styles.container}> Tehtävä 1 </Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -15,6 +25,20 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#D5484F',
         padding: 20
+
         
-    }
+    },
+    floatingButton2: {
+        borderWidth: 5,
+        borderColor: 'rgba(0,0,0,0.2)',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        bottom: 130,
+    
+        width: 300,
+        height: 60,
+        backgroundColor: '#fff',
+        borderRadius: 100,
+      }
 })
